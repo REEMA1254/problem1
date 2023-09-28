596487034414bd90805cfffe7fee1f4f8a70a33d
maryam <Reemaali9048@gmail.com>
2023-09-28 10:23:46 +0300

    complete assigment

--- gradings.js/index.js
+++ gradings.js/index.js
@@ -1,18 +1,30 @@
-calculate_grade() 
- let marks = float(input("student mark" (0-100): "))
-    if 0 <= marks <= 100:
-    if marks > 79
-    grade = "A"
-else  if 60 <= marks <= 79:
-    grade = "B"
-else  if 50 <= marks <= 59:
-    grade = "C"
-else  if 40 <= marks <= 49:
-    grade = "D"
-else:
-    grade = "E"
+function calculate_grade(){}
 
-else: ("Marks should be between 0 and 100.")
+    let marks = 0<=100;
+
+        if 0 <= marks <= 100:
+            if marks > 79:
+                grade = "A"
+            else if 60 <= marks <= 79:
+                grade = "B"
+            else if 50 <= marks <= 59:
+                grade = "C"
+            else if 40 <= marks <= 49:
+                grade = "D"
+            else:
+                grade = "E"
+            
+            
+
+
  
-//calculate_grade()
+
+
+
+
+
+
+
+
+
 
