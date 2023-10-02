3252ef3b730cfb3771631cf237d0939d1e116531
maryam <Reemaali9048@gmail.com>
2023-10-02 10:35:36 +0300

    Calculates the grade

---
+++ Grading/grading .js
@@ -0,0 +1,19 @@
+// Function : Calculates grade
+// the students grades
+function calculateGrade(marks) {
+    if (marks > 79 &&marks<= 100) {
+      grade = 'A';
+    } else if (marks >= 60 && marks <= 79) {
+      grade = 'B';
+    } else if (marks >= 50 && marks <= 59) {
+      grade = 'C';
+    } else if (marks >= 40 && marks <= 49) {
+      grade = 'D';
+    } else {
+      grade = 'E';
+    
+    return 'Invalid input.Marks should be between 0 and 100.';
+  }
+}
+  module.exports = calculateGrade;
+  
